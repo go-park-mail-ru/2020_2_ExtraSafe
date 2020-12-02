ece7a0f7817714726dde98d3522d684584392ad9
pringleskate <kattyseskimojoe@gmail.com>
2020-12-02 15:39:08 +0300

    Тесты на services/boards

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo/v4 v4.1.17
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/lib/pq v1.8.0
 	github.com/rs/zerolog v1.20.0
--- internal/services/boards/service_test.go
+++ internal/services/boards/service_test.go
@@ -1,6 +1,70 @@
 package boards
 
-/*func TestService_CreateBoard(t *testing.T) {
+import (
+	"context"
+	"github.com/go-park-mail-ru/2020_2_ExtraSafe/internal/models"
+	"github.com/go-park-mail-ru/2020_2_ExtraSafe/internal/tools/validation"
+	"github.com/go-park-mail-ru/2020_2_ExtraSafe/services/mock"
+	protoBoard "github.com/go-park-mail-ru/2020_2_ExtraSafe/services/proto/board"
+	protoProfile "github.com/go-park-mail-ru/2020_2_ExtraSafe/services/proto/profile"
+	"github.com/golang/mock/gomock"
+	"reflect"
+	"testing"
+)
+
+func TestService_CreateBoard(t *testing.T) {
+	request := &protoBoard.BoardChangeInput{
+		UserID:    1,
+		BoardID:   1,
+		BoardName: "name",
+		Theme:     "dark",
+		Star:      false,
+	}
+
+	input := models.BoardChangeInput{
+		UserID:    request.UserID,
+		BoardID:   request.BoardID,
+		BoardName: request.BoardName,
+		Theme:     request.Theme,
+		Star:      request.Star,
+	}
+
+	internal := models.BoardOutsideShort{
+		BoardID: input.BoardID,
+		Name:    input.BoardName,
+		Theme:   input.Theme,
+		Star:    input.Star,
+	}
+
+	expect := &protoProfile.BoardOutsideShort{
+		BoardID: internal.BoardID,
+		Name:    internal.Name,
+		Theme:   internal.Theme,
+		Star:    internal.Star,
+	}
+
+	ctrlBoard := gomock.NewController(t)
+	defer ctrlBoard.Finish()
+	mockBoardService := mock.NewMockBoardClient(ctrlBoard)
+
+	validator := validation.NewService()
+
+	service := NewService(mockBoardService, validator)
+
+	mockBoardService.EXPECT().CreateBoard(context.Background(), request).Return(expect, nil)
+
+	board, err := service.CreateBoard(input)
+	if err != nil {
+		t.Errorf("unexpected err: %s", err)
+		return
+	}
+	if !reflect.DeepEqual(board, internal) {
+		t.Errorf("results not match, want \n%+v, \nhave \n%+v", internal, board)
+		return
+	}
+}
+
+func TestService_ChangeBoard(t *testing.T) {
 	request := &protoBoard.BoardChangeInput{
 		UserID:    1,
 		BoardID:   1,
@@ -39,15 +103,6 @@ package boards
 
 	service := NewService(mockBoardService, validator)
 
-	mockBoardService.EXPECT().CreateBoard(context.Background(), request).Return(expect, nil)
+	mockBoardService.EXPECT().ChangeBoard(context.Background(), request)
 
-	board, err := service.CreateBoard(input)
-	if err != nil {
-		t.Errorf("unexpected err: %s", err)
-		return
-	}
-	if !reflect.DeepEqual(board, internal) {
-		t.Errorf("results not match, want \n%v, \nhave \n%v", internal, board)
-		return
-	}
-}*/
+}
