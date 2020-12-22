734ab0a43107cb49015eef733633775af5b9ad04
Aleksandr Dergachev <smaild-m@yandex.ru>
2020-12-22 19:41:39 +0300

    добавил https в cors

--- cmd/handlers/middlewares/middlew.go
+++ cmd/handlers/middlewares/middlew.go
@@ -55,8 +55,8 @@ func (m middlew) CORS() echo.MiddlewareFunc {
 			"http://127.0.0.1:63246",
 			"http://127.0.0.1",
 			"http://tabutask.ru",
-			"https://tabutask.ru",
-			"https://127.0.0.1"},
+			"https://tabutask.ru:443",
+			"https://127.0.0.1:443"},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, "X-CSRF-Token"},
 		AllowCredentials: true,
 	})
